24c40b1b03f785db2ac4bc7548136d3f36516808
RahmaAhmed22 <ahmedrahma589@gmail.com>
2026-04-03 11:15:44 +0200

    rt configured

--- main.tf
+++ main.tf
@@ -17,7 +17,21 @@ resource "aws_internet_gateway" "gw" {
   tags = {
     Name = "abc-igw"
   }
-} 
+}
+
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+
+  tags = {
+    Name = "abc-rt"
+  }
+}
 
 resource "aws_subnet" "subnet1" {
   vpc_id            = aws_vpc.main.id
